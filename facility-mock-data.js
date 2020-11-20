853a23883213b9797efceeaa15795ea0978bd23b
Emanuel Steininger <emanuel.steininger@eversports.com>
2020-11-20 11:44:13 +0100

    Create facility-mock-data.js
    Use this example response for the exercise to mock the data.

---
+++ facility-mock-data.js
@@ -0,0 +1,69 @@
+const facilities = [
+ {
+   id: 1001,
+   name: 'Soccerdome Wien 10',
+   street: 'Kauerhof 4',
+   zip: 1110,
+   city: 'Wien',
+   latitude: 48,1624984741,
+   longitude: 16,3386001587,
+   createdAt: '2018-01-01 10:00',
+   courts: [
+     {
+       id: 2001,
+       name: 'Court 1',
+     },
+     {
+       id: 2002,
+       name: 'Court 2',
+     },
+   ]
+ },
+ {
+   id: 1002,
+   name: 'LTM Tennis',
+   street: 'Brunnengasse 99',
+   zip: 1010,
+   city: 'Wien',
+   latitude: 48,2075004578,
+   longitude: 16,4281005859,
+   createdAt: '2018-01-05 10:00',
+   courts: [
+     {
+       id: 2005,
+       name: 'Court 1',
+     },
+     {
+       id: 2006,
+       name: 'Court 2',
+     },
+     {
+       id: 2007,
+       name: 'Court 3',
+     },
+     {
+       id: 2008,
+       name: 'Court 4',
+     },
+     {
+       id: 2009,
+       name: 'Court 5',
+     },
+     {
+       id: 2010,
+       name: 'Court 6',
+     },
+   ]
+ },
+ {
+   id: 1003,
+   name: 'Yoga Studio',
+   street: 'Kellergasse 14',
+   zip: 1210,
+   city: 'Wien',
+   latitude: 48,2127990723,
+   longitude: 16,3701992035,
+   createdAt: '2018-07-01 10:00',
+   courts: []
+ },
+]
